b3a4deb1a8290e57798d80ec21c44fdd3b97e3b7
Gjelbrim Klobocishta <jebklob01@gmail.com>
2022-11-30 22:55:15 -0500

    first two functions are done.

---
+++ Script.ts
@@ -0,0 +1,66 @@
+// write a function that takes the input string and reverses it
+function reverseThisString(string){
+    return string.split("").reverse().join("");
+    // the way this works is it splits the string in to chars sets up a array flips it then joins it
+}
+
+
+
+// write a function that takes the input string and switches all uppercase characters to lowercase and lowercase charcaters to uppercase
+// example:
+// argument: Hello World
+// return: hELLO wORLD
+function swapCase(string) {
+    let test = ""
+    for(let i = 0; i<string.length; i++){
+        if(string[i] === string[i].toLowerCase()){
+            test += string[i].toUpperCase();
+        }else {
+            test += string[i].toLowerCase();
+        }
+    }
+    return test;
+}
+
+console.log(swapCase("HeLLo"))
+//convert array of numbers from farenheit to celcius
+// example:
+// argument: [23, 32, 41, 50, 59]
+// return: [-5, 0, 5, 10, 15]
+// hint: use Array.map
+function toCelcius(array){
+
+}
+
+
+//write a function that takes an input array an returns an array of booleans (>=75) or fail (<75)
+// example:
+// argument: [20, 30, 50, 80, 90, 100]
+// return: [false, false, false, true, true, true]
+// hint: use Array.map
+function passOrFail(array){
+
+}
+
+
+//write code that loops through the two variables returns an array ['2 is zwei', '3 is drei', '4 is vier', '5 is fünf', '6 is sechs']
+// example:
+// return: ['2 is zwei', '3 is drei', '4 is vier', '5 is fünf', '6 is sechs']
+// hint: use Array.forEach, Array.map or a for loop
+function germanNumbers(){
+    const cardinalNumbers = [2,3,4,5,6];
+    const germanNumbers = ['zwei', 'drei', 'vier', 'fünf', 'sechs'];
+
+}
+
+
+// write code that returns an array of ONLY prime numbers that are in the array numbers
+// example: 
+// return [2, 3, 5, 7, 11, 13]
+// hint use: Array.filter
+function returnPrimeNumbers(){
+    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+}
+
+//Write a function that loops through and console.log's the numbers from 1 to 100, except multiples of three, log (without quotes) "CSC225 RULES" instead of the number, for the multiples of five, log (without quotes) "I LOVE JAVASCRIPT". For numbers which are multiples of both three and five, log (without quotes) "CSC225 RULES I LOVE JAVASCRIPT" Console log out:
